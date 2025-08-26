47e95e6bbcc97a6693e031d0690a210853088ad7
TatiParra21 <tatiparras21@gmail.com>
2025-08-26 14:30:10 -0500

    oth

--- backend/src/db.ts
+++ backend/src/db.ts
@@ -1,8 +1,8 @@
 // backend/src/db.ts
 import { Pool } from 'pg'
-
+import dns from 'dns';
 console.log("DATABASE_URL:", process.env.DATABASE_URL);
-
+dns.setDefaultResultOrder('ipv4first');
 export const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
    ssl: {
