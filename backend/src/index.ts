7d9994ebcf763ea406d1048a06fe5320f783e0b0
TatiParra21 <tatiparras21@gmail.com>
2025-09-02 15:58:17 -0500

    aig

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -14,7 +14,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors({
-  origin: "https://colorcchemescgenerator.netlify.app/badges",
+  origin: "*",
   methods: ["GET", "POST"],
   credentials: true
 }));
