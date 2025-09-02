3c9c765f091bef173f21eb212fa0d895047ca088
TatiParra21 <tatiparras21@gmail.com>
2025-09-02 16:01:54 -0500

    .env upfate

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -13,11 +13,7 @@ import router from "./colors"
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors({
-  origin: "*",
-  methods: ["GET", "POST"],
-  credentials: true
-}));
+app.use(cors());
 app.use(express.json());
 app.use("/api",router)
 app.use((req,res)=>{
